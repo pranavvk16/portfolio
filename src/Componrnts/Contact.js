c5404723704f2b17c0c2335a671a1942bcd2aa10
Pranav VK <pranavvk16@gmail.com>
2021-10-29 16:50:40 +0530

    z-index

--- src/Componrnts/Contact.js
+++ src/Componrnts/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
             <h2>Say Hello</h2><br />
             <div className="username">What is your name?<br /> <input type="text" /></div>
             <div className="usermail">Your email<br /> <input type="email" /> </div>
-            <div className="usermsg">Whatsapp!!<br /><textarea type="text" /></div><br />
+            <div className="usermsg">What's on your Mind<br /><textarea type="text" /></div><br />
             <button className="send">Get in Touch</button>
         </div>
     )
